9fd3e12d6c01fc5f198e50d1e5467151a9bb6b04
Ivy <yunjilim@gmail.com>
2019-03-04 11:34:45 -0800

    update logger

--- middleware/logger.js
+++ middleware/logger.js
@@ -1,21 +1,10 @@
-const moment = require('moment');
+const moment = require("moment");
 
-<<<<<<< HEAD
-// req and res are objects
-=======
-// Date.now()
-// moment()
+// Date.now() = moment()
 
-// req, res are objects
->>>>>>> c24f5416adae537c618cfe70e39c44409c66d033
-// next is a callback function
-const logger = (req, res, next) => {
-  console.log(`${req.url} was requested at ${moment()}`);
-  next();
+const logger = (request, response, next) => {
+	console.log(`${request.url} was requested at ${moment()}`);
+	next();
 };
 
-<<<<<<< HEAD
 module.exports = logger;
-=======
-module.exports = logger;
->>>>>>> c24f5416adae537c618cfe70e39c44409c66d033
